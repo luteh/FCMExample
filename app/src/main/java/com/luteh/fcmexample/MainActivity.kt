dffe6ec1f4ed77ac0f8c9fbd7f0a736518c56749
luteh <luthfanmaftuh@gmail.com>
2019-05-15 22:09:34 +0700

    Testing firebase crashlytics

--- app/src/main/java/com/luteh/fcmexample/MainActivity.kt
+++ app/src/main/java/com/luteh/fcmexample/MainActivity.kt
@@ -2,11 +2,31 @@ package com.luteh.fcmexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import com.crashlytics.android.Crashlytics
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            Crashlytics.log(Log.DEBUG, "tag", "Error crash testing 3")
+//            Crashlytics.logException(Throwable("Error crash testing 2"))
+//            Crashlytics.getInstance().crash() // Force a crash
+//            Crashlytics.getInstance()
+        }
+
+        addContentView(
+            crashButton, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
     }
 }
